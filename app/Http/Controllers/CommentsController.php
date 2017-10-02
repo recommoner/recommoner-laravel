aec793033312dfa9ebabb9f82db1297ac2c7e7a6
Sammar Abbas <sammarabbas152@gmail.com>
2017-10-02 20:54:57 +0500

    update

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\DB;
 
 class CommentsController extends Controller
 {
+    function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
