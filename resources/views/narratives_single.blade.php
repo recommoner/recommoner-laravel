ca81ae14e89c2cf1f6b18fe238940a3032fc27ba
Sameer Ali <info@iridiumsoft.org>
2017-10-21 05:06:14 +0500

    small changes

--- resources/views/narratives_single.blade.php
+++ resources/views/narratives_single.blade.php
@@ -50,8 +50,8 @@
         <div class="form-group">
             <h3>Comments</h3>
             @if (Auth::guest())
-            <p>Please <a href="{{url('login?redirect='.Request::path())}}">sign in</a> or <a
-                        href="{{url('register?redirect='.Request::path())}}">create an account</a> to
+            <p>Please <a href="{{url('login?redirect='.Request::path())}}#comments">sign in</a> or <a
+                        href="{{url('register?redirect='.Request::path())}}#comments">create an account</a> to
                 post a comment</p>
             @endif
 
